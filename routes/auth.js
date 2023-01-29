9f57b1e2bbd84bd19fd62708e5762fa7c491d8ec
Pobarul-islam <pobarul3@gmail.com>
2023-01-30 00:55:17 +0600

    register api created

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,28 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 // Register
 
-router.get('/register', async (req, res) => {
-  const user = await new User({
-    username: 'nil',
-    email: 'nil@gmail.com',
-    password: '123456',
-  });
+router.post('/register', async (req, res) => {
+  try {
+    // generate password
 
-  await user.save();
-  res.send('ok');
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    // create new password
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    // save user and response
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
