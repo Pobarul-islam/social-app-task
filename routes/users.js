72f53240267171d3ce65be397a10e24388970343
Pobarul-islam <pobarul3@gmail.com>
2023-01-30 20:16:05 +0600

    follow and following added

--- routes/users.js
+++ routes/users.js
@@ -46,8 +46,54 @@ router.delete('/:id', async (req, res) => {
 
 
 // get a user
+
+
+router.get("/id:", async(req, res)=>{
+    try{
+
+        const user = await User.findById(req.params.id)
+        const {password, updateAt, ...other} = user._doc
+        res.status(200).json(other);
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
 // get all user
+
+
 // follow a user
+
+
+
+router.put("/:id/follow", async(req, res)=>{
+    if(req.body.userId !== req.params.id){
+        try{
+
+            const user = await User.findById(req.params.id)
+            const currentUser = await User.findById(req.body.userId)
+            if(!user.followers.includes(req.body.userId)){
+                await user.updateOne({$push:{followers:req.body.userId}})
+                res.status(200).json("user has been followed")
+                await currentUser.updateOne({
+                  $push: { followings: req.params.id },
+                });
+
+            }else{
+                res.status(403).json("You already follow this user")
+            }
+
+        }catch(err){
+            res.status(500).json(err)
+        }
+
+    }else{
+        res.status(403).json("you can't follow your self")
+    }
+})
 // unfollow a user
+
+
+
 
 module.exports = router;
