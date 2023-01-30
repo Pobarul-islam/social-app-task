fdfb0f218b309f5fb6f5270cdd3d92f549019316
Pobarul-islam <pobarul3@gmail.com>
2023-01-30 20:28:19 +0600

    unfollow api created

--- routes/users.js
+++ routes/users.js
@@ -91,9 +91,32 @@ router.put("/:id/follow", async(req, res)=>{
         res.status(403).json("you can't follow your self")
     }
 })
+
+
 // unfollow a user
 
 
+router.put('/:id/unfollow', async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $pull: { followers: req.body.userId } });
+        res.status(200).json('user has been unfollowed');
+        await currentUser.updateOne({
+          $pull: { followings: req.params.id },
+        });
+      } else {
+        res.status(403).json('You dont follow this user');
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("you can't unfollow your self");
+  }
+});
 
 
 module.exports = router;
